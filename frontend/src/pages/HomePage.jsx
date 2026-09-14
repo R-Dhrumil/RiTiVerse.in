baf7cd239d41b68aef63ff27e4396a5f83e6197b
R-Dhrumil <dhrumilrana516@gmail.com>
2026-09-14 12:34:34 +0530

    rm: why us section

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Hero from '../components/Hero';
 import AdminPreviewSection from '../components/AdminPreviewSection';
 import Services from '../components/Services';
-import WhyChooseUs from '../components/WhyChooseUs';
 import Portfolio from '../components/Portfolio';
 import Pricing from '../components/Pricing';
 import Testimonials from '../components/Testimonials';
@@ -19,9 +18,6 @@ export default function HomePage() {
 
       {/* Services Overview */}
       <Services />
-
-      {/* Why Choose Us */}
-      <WhyChooseUs />
 
       {/* Portfolio Highlight */}
       <Portfolio />
